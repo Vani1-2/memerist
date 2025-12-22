fd64d0e8c2b92df3ca08301f1943abd83306940c
Giovanni <giovannirafanan609@gmail.com>
2025-12-22 14:22:20 +0800

    added deb files to curl

--- deb-build.sh
+++ deb-build.sh
@@ -2,6 +2,6 @@
 set -e
 
 distrobox enter ubuntu -- bash -c '
-  cp ~/Projects/practice-gnome-app/build/src/memerist ~/Projects/ubuntubox/memerist-0.1.0-1-amd64/usr/bin/
-  dpkg-deb --build ~/Projects/ubuntubox/memerist-0.1.0-1-amd64
+  cp build/src/memerist deb-build/memerist-amd64/usr/bin/
+  dpkg-deb --build deb-build/memerist-amd64
 '
